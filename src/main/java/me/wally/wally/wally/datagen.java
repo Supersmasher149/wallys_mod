e7407a876b1cb9d3f14895d277c2210320d3f53d
Wally <walter.gibbons@gmail.com>
2021-04-04 13:50:51 -0400

    datagen2

--- src/main/java/me/wally/wally/wally/datagen.java
+++ src/main/java/me/wally/wally/wally/datagen.java
@@ -3,5 +3,5 @@ package me.wally.wally.wally;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 
 public class datagen {
-    public void gatherData(GatherDataEvent)
+    //public void gatherData(GatherDataEvent)
 }
